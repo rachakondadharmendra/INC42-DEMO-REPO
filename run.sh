61949ba754f5b463d7b0d234ef2372697e508fb3
RACHAKONDA DHARMENDRA <146170291+rachakondadharmendra@users.noreply.github.com>
2024-05-20 20:06:46 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,19 +5,23 @@ ecr_repo=$1
 
 get_latest_image() {
     latest_image=$(aws ecr describe-images --repository-name $ecr_repo --query 'sort_by(imageDetails,& imagePushedAt)[-1].imageTags[0]' --output text)
-    echo "$ecr_repo:$latest_image"
+    echo "$latest_image"
 }
 
 replace_ecr_tag() {
     local compose_file="$1"
     local ecr_repo="$2"
-    local latest_image="$3"
+    local new_tag="$3"
+    
+    local current_image="${aws_tag}${ecr_repo}:"
 
-    sed -i "s/$ecr_repo:[^ ]*/$latest_image/g" "$compose_file"
+    # Use sed to replace only the tag part of the image
+    sed -i "s|${current_image}[^ ]*|${current_image}${new_tag}'|g" "$compose_file"
 }
 
-latest_image=$(get_latest_image)
-echo "Latest image in ECR: $latest_image"
+latest_tag=$(get_latest_image)
+echo "Latest tag in ECR: $latest_tag"
+aws_tag="079711632559.dkr.ecr.ap-south-2.amazonaws.com/"
 
-replace_ecr_tag "$compose_file" "$ecr_repo" "$latest_image"
+replace_ecr_tag "$compose_file" "$ecr_repo" "$latest_tag"
 echo "Updated Docker Compose file with latest image tag."
